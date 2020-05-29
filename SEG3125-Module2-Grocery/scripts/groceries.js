83230acc2cdc10ee6d53c8c4dd244c949f11bb6e
KDube1 <krishna.dube1@gmail.com>
2020-05-28 20:37:38 -0400

    completed lab

--- SEG3125-Module2-Grocery/scripts/groceries.js
+++ SEG3125-Module2-Grocery/scripts/groceries.js
@@ -1,26 +1,79 @@
-	
+import {preferences} from "./main.js";
 // Array of products, each product is an object with different fieldset
 // A set of ingredients should be added to products		 
 
 var products = [
 	{
+		name: "tomato",
+		vegetarian: true,
+		glutenFree: true,
+		organic: true,
+		price: "1.50"
+	},
+	{
 		name: "brocoli",
 		vegetarian: true,
 		glutenFree: true,
-		price: 1.99
+		organic: true,
+		price: "1.99"
 	},
 	{
 		name: "bread",
 		vegetarian: true,
 		glutenFree: false,
-		price: 2.35
+		organic: false,
+		price: "2.35"
+	},
+	{
+		name: "croissant",
+		vegetarian: true,
+		glutenFree: false,
+		organic: false,
+		price: "3.00"
+	},
+	{
+		name: "butter",
+		vegetarian: true,
+		glutenFree: true,
+		organic: false,
+		price: "3.50"
+	},
+	{
+		name: "raddish",
+		vegetarian: true,
+		glutenFree: true,
+		organic: true,
+		price: "5.00"
+	},
+	{
+		name: "cereal",
+		vegetarian: true,
+		glutenFree: false,
+		organic: false,
+		price: "6.00"
+	},
+	{
+		name: "steak",
+		vegetarian: false,
+		glutenFree: true,
+		organic: true,
+		price: "8.00"
 	},
 	{
 		name: "salmon",
 		vegetarian: false,
 		glutenFree: true,
-		price: 10.00
+		organic: true,
+		price: "10.00"
+	},
+	{
+		name: "pork bun",
+		vegetarian: false,
+		glutenFree: false,
+		organic: false,
+		price: "15.00"
 	}
+
 ];
 	
 
@@ -28,29 +81,32 @@ var products = [
 // given restrictions provided, make a reduced list of products
 // prices should be included in this list, as well as a sort based on price
 
-function restrictListProducts(prods, restriction) {
+export function restrictListProducts() {
+
 	let product_names = [];
-	for (let i=0; i<prods.length; i+=1) {
-		if ((restriction == "Vegetarian") && (prods[i].vegetarian == true)){
-			product_names.push(prods[i].name);
+	for (let i=0; i<products.length; i+=1) {
+		if (preferences["vegetarian"] && !products[i].vegetarian){
+			continue;
 		}
-		else if ((restriction == "GlutenFree") && (prods[i].glutenFree == true)){
-			product_names.push(prods[i].name);
+		if (preferences["glutenFree"] && !products[i].glutenFree){
+			continue;
 		}
-		else if (restriction == "None"){
-			product_names.push(prods[i].name);
+		if (preferences["organic"] && !products[i].organic){
+			continue;
 		}
+		product_names.push(products[i].name +" $" + products[i].price);
 	}
 	return product_names;
 }
 
 // Calculate the total price of items, with received parameter being a list of products
-function getTotalPrice(chosenProducts) {
-	totalPrice = 0;
-	for (let i=0; i<products.length; i+=1) {
-		if (chosenProducts.indexOf(products[i].name) > -1){
-			totalPrice += products[i].price;
+export function getTotalPrice(chosenProducts) {
+	let totalPrice = 0;
+	for (let i=0; i<chosenProducts.length; i++) {
+			let currPrice = chosenProducts[i].match("\\d[0-9.]*");
+			//console.log(chosenProducts[i])
+			console.log(currPrice[0])
+			totalPrice += parseFloat(currPrice);
 		}
-	}
 	return totalPrice;
 }
--- SEG3125-Module2-Grocery/scripts/main.js
+++ SEG3125-Module2-Grocery/scripts/main.js
@@ -2,17 +2,30 @@
 // This function is called when any of the tab is clicked
 // It is adapted from https://www.w3schools.com/howto/howto_js_tabs.asp
 
+
+import {restrictListProducts,getTotalPrice} from "./groceries.js";
+
+
+document.getElementById('clientTab').addEventListener('click', event => openInfo(event, 'Client'));
+document.getElementById('productTab').addEventListener('click', event => openInfo(event, 'Products'));
+document.getElementById('cartTab').addEventListener('click', event => openInfo(event, 'Cart'));
+
+document.getElementById('addCart').addEventListener('click', () => selectedItems());
+
+
 function openInfo(evt, tabName) {
 
 	// Get all elements with class="tabcontent" and hide them
-	tabcontent = document.getElementsByClassName("tabcontent");
-	for (i = 0; i < tabcontent.length; i++) {
+
+	let tabcontent = document.getElementsByClassName("tabcontent");
+
+	for (let i = 0; i < tabcontent.length; i++) {
 		tabcontent[i].style.display = "none";
 	}
 
 	// Get all elements with class="tablinks" and remove the class "active"
-	tablinks = document.getElementsByClassName("tablinks");
-	for (i = 0; i < tablinks.length; i++) {
+	let tablinks = document.getElementsByClassName("tablinks");
+	for (let i = 0; i < tablinks.length; i++) {
 		tablinks[i].className = tablinks[i].className.replace(" active", "");
 	}
 
@@ -23,25 +36,27 @@ function openInfo(evt, tabName) {
 }
 
 
+
 	
 // generate a checkbox list from a list of products
 // it makes each product name as the label for the checkbos
 
-function populateListProductChoices(slct1, slct2) {
-    var s1 = document.getElementById(slct1);
-    var s2 = document.getElementById(slct2);
+function populateListProductChoices() {
+    var s2 = document.getElementById("displayProduct");
+
 	
 	// s2 represents the <div> in the Products tab, which shows the product list, so we first set it empty
     s2.innerHTML = "";
 		
 	// obtain a reduced list of products based on restrictions
-    var optionArray = restrictListProducts(products, s1.value);
+    var optionArray = restrictListProducts();
 
 	// for each item in the array, create a checkbox element, each containing information such as:
 	// <input type="checkbox" name="product" value="Bread">
 	// <label for="Bread">Bread/label><br>
-		
-	for (i = 0; i < optionArray.length; i++) {
+
+
+	for (let i = 0; i < optionArray.length; i++) {
 			
 		var productName = optionArray[i];
 		// create the checkbox and add in HTML DOM
@@ -78,7 +93,7 @@ function selectedItems(){
 	var para = document.createElement("P");
 	para.innerHTML = "You selected : ";
 	para.appendChild(document.createElement("br"));
-	for (i = 0; i < ele.length; i++) { 
+	for (let i = 0; i < ele.length; i++) {
 		if (ele[i].checked) {
 			para.appendChild(document.createTextNode(ele[i].value));
 			para.appendChild(document.createElement("br"));
@@ -88,7 +103,30 @@ function selectedItems(){
 		
 	// add paragraph and total price
 	c.appendChild(para);
-	c.appendChild(document.createTextNode("Total Price is " + getTotalPrice(chosenProducts)));
+	c.appendChild(document.createTextNode("Total Price is $" + getTotalPrice(chosenProducts)));
 		
 }
+
+export  var preferences = {
+	vegetarian: false,
+	glutenFree: false,
+	organic: false,
+
+};
+
+
+
+
+document.getElementById('vegetarian').addEventListener("change", () =>checkBoxClicked('vegetarian'))
+document.getElementById('glutenFree').addEventListener("change", () =>checkBoxClicked('glutenFree'))
+document.getElementById('organic').addEventListener("change", () =>checkBoxClicked('organic'))
+
+function checkBoxClicked(checkBoxString){
+	preferences[checkBoxString] = !preferences[checkBoxString]
+
+	populateListProductChoices();
+
+}
+
+
 
